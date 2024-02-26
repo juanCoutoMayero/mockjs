1556ddb26ed10f5f6d008e032fb71434a9e54a0b
Juan Diego Couto Mayero <jcouto@bps.gub.uy>
2024-02-26 20:05:39 -0300

    Add route

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const http = require('http');
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const router = express();
+
+const server = http.createServer(router);
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.use('/', routes);
+
+router.use('/auth/login', async (req, res, next),() => {
+  authController(req, res, next);
 });
+
+router.post('/auth/login', validationRequest(IRequestAuthLogin), async (req, res), async () => {
+  const request = req.body;
+  res.status(response.status).json({auth_token: 'asfnaskdjnzfskdzf'});
+});
+
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
